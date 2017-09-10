ba37169746e54d73270a4713fcb1ad9f05a22656
praneethmutnuru8 <praneethmutnuru@gmail.com>
2017-09-10 22:14:10 +0530

    Secondary commit

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,13 +8,39 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+$(document).ready(function() {
+    $("#contact-form").submit(function(e){
+                 document.getElementById("ajax-response1").style.display = "none";
+                 
+                 var name= $("#inputEmail").val();
+                 var email= $("#inputName").val();
+                 var message= $("#inputMessage").val();
+                 if (email == "" || name="" || email=" "){
+                  document.getElementById("ajax-response2").style.display = "block";
+                 }
+                 else{
+                  document.getElementById("ajax-response1").style.display = "block";
+                  $("#inputEmail").val('');
+                  $("#inputName").val('');
+                  $("#inputMessage").val('');
+                  
+                 // $.ajax({
+                  //     type: 'post',
+                  //     url: 'process.php',
+                  //    data: $('form').serialize(),
+                  //      success: function () {
+                  //       alert('form was submitted');
+                  //            }
+                  //       });
+
+
+                }
+
+               e.preventDefault();
+});
+}
 function validateForm(){
 
- alert('Thank you for your respnse.');
-}
-
-function validateName()
-{
-
-
+ alert('Thank you for your response.');
 }
