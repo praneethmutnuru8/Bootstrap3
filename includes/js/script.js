fc997c060a653fb73dc98ae7e549cf354ecd2a27
praneethmutnuru8 <praneethmutnuru@gmail.com>
2017-09-11 14:51:09 +0530

    third commit

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,46 +1,17 @@
-/*
 
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
-
-$(document).ready(function() {
-    $("#contact-form").submit(function(e){
-                 document.getElementById("ajax-response1").style.display = "none";
-                 
-                 var name= $("#inputEmail").val();
-                 var email= $("#inputName").val();
-                 var message= $("#inputMessage").val();
-                 if (email == "" || name="" || email=" "){
-                  document.getElementById("ajax-response2").style.display = "block";
-                 }
-                 else{
-                  document.getElementById("ajax-response1").style.display = "block";
-                  $("#inputEmail").val('');
-                  $("#inputName").val('');
-                  $("#inputMessage").val('');
-                  
-                 // $.ajax({
-                  //     type: 'post',
-                  //     url: 'process.php',
-                  //    data: $('form').serialize(),
-                  //      success: function () {
-                  //       alert('form was submitted');
-                  //            }
-                  //       });
-
-
-                }
-
-               e.preventDefault();
-});
-}
 function validateForm(){
 
  alert('Thank you for your response.');
 }
+
+function userid_validation(inputName)  
+{  
+  var inputName_len = inputName.value.length;  
+ if (inputName_len == 0 || inputName_len >= 6 || inputName_len < 12)  
+  {  
+	alert("User Id should not be empty / length be between "+6+" to "+12);  
+	uid.focus();  
+	return false;  
+   }  
+  return true;  
+} 
